6022f5e0cf91fea5c22622834892a15306f67e30
Julian Grey <julianrgrey@gmail.com>
2015-05-18 15:38:48 +0100

    Add Contact class

---
+++ Contacts/Contact.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class Contact {
+private:
+	std::string firstName;
+	std::string lastName;
+	int strSize;
+public:
+	void setFirstName(std::string name) {
+		this->firstName = name;
+	}
+	std::string getFirstName() {
+		return this->firstName;
+	}
+
+	void setLastName(std::string name) {
+		this->lastName = name;
+	}
+	std::string getLastName() {
+		return this->lastName;
+	}
+
+	std::string getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	int getNameLength() {
+		return this->getFullName().size();
+	}
+};
